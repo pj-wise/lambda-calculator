ca9a24fe78944c4c43a35b77e1f72af8d8f737f7
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-13 21:17:09 -0400

    forgot about display/display props. mvp met, copied styling to save time

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = props => {
+  return <div className="outputBox">{props.number}</div>;
 };
 
 export default Display;
